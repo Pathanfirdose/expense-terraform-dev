ee3cc3205b3a0124be2918e6e60fbe7b66f6874d
sivakumarreddym <info@joindevops.com>
2024-09-26 10:11:18 +0530

    user profile

--- 20-sg/main.tf
+++ 20-sg/main.tf
@@ -8,12 +8,3 @@ module "mysql_sg" {
     sg_tags = var.mysql_sg_tags
 }
 
-module "backend_sg" {
-    source = "../../terraform-aws-security-group"
-    project_name = var.project_name
-    environment = var.environment
-    sg_name = "backend"
-    vpc_id = local.vpc_id
-    common_tags = var.common_tags
-    sg_tags = var.backend_sg_tags
-}
