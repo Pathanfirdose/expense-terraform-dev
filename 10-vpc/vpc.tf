4b3472951a601e8a5ebdf2495e94a73ca10deee8
sivakumarreddym <info@joindevops.com>
2024-09-26 09:58:19 +0530

    user profile

--- 10-vpc/vpc.tf
+++ 10-vpc/vpc.tf
@@ -10,17 +10,3 @@ module "vpc" {
     database_subnet_cidrs = var.database_subnet_cidrs
     is_peering_required = var.is_peering_required
 }
-
-
-module "vpc" {
-    #source = "../terraform-aws-vpc"
-    source = "git::https://github.com/daws-81s/terraform-aws-vpc.git?ref=main"
-    vpc_cidr = var.backend_vpc_cidr
-    project_name = var.project_name
-    environment = var.environment
-    common_tags = var.common_tags
-    public_subnet_cidrs = var.public_subnet_cidrs
-    private_subnet_cidrs = var.private_subnet_cidrs
-    database_subnet_cidrs = var.database_subnet_cidrs
-    is_peering_required = var.is_peering_required
-}
